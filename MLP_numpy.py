4df7e2dfbe4adaba9869eeaa5f50e2ae2e3e5d2b
seblful <alesha.chimba@gmail.com>
2024-01-05 10:35:41 +0300

    first commit

---
+++ MLP_numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class MLP:
+    def __init__(self):
+        self.num_neurons = 3
+        self.num_layers = 3
+        self.weight_size = None
+        self.lr = 0.01
+        self.num_epochs = 1000
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+    def train(self):
+        pass
