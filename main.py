a6995bf2a51e76e090deb554d62f3affd9c4ab51
seblful <alesha.chimba@gmail.com>
2024-01-09 20:38:51 +0300

    [ADD] main script to train

---
+++ main.py
@@ -0,0 +1,27 @@
+from MLP_numpy import MLP
+import mnist
+
+
+def create_mnist():
+    # mnist.init()
+    x_train, y_train, x_test, y_test = mnist.load()
+
+    return x_train, y_train, x_test, y_test
+
+
+def main():
+
+    x_train, y_train, x_test, y_test = create_mnist()
+    mlp = MLP(train_data=x_train[:1000, :],
+              train_labels=y_train[:1000],
+              val_data=x_test[:500, :],
+              val_labels=y_test[:500])
+
+    mlp.train()
+
+    prediction = mlp.predict(x_train[1002, :])
+    print(prediction)
+
+
+if __name__ == "__main__":
+    main()
